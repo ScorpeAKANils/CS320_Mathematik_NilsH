64e81f06cbe5faa7b5855c5cf18844ea1bc6b068
NilsMac <nils.henningsen@freenet.de>
2024-07-12 20:02:54 +0200

    Aufgabe 2

--- Assets/Aufgabe1.cs
+++ Assets/Aufgabe1.cs
@@ -16,13 +16,36 @@ public class Aufgabe1 : MonoBehaviour
 {
     public float frequenz;
     public float WaveHeight;
-    public float speed = 1; 
+    public float speed = 1;
+    public Aufgaben curAuf = Aufgaben.einsA;
+    [Range(0, 3)]
+    public float slopeSpeedFactor=1;
+
     private void Update()
     {
-        transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y+AufgabeEinsA()*speed*Time.deltaTime, transform.position.z); 
+        switch (curAuf)
+        {
+            case Aufgaben.einsA:
+                AufgabeEinsA();
+                break;
+            case Aufgaben.einsB:
+                AufgabeEinsB();
+                break;
+
+        }
     }
-    public float AufgabeEinsA()
+    public void AufgabeEinsA()
     {
-        return Mathf.Sin(transform.position.x +frequenz)*WaveHeight; 
+        float newYPos =  Mathf.Sin(transform.position.x +frequenz)*WaveHeight;
+        transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y + newYPos * speed * Time.deltaTime, transform.position.z);
+    }
+
+    public void AufgabeEinsB()
+    {
+        float newYPos = Mathf.Sin((transform.position.x + Time.time * speed) * frequenz) * WaveHeight;
+        float yAbleitung = Mathf.Cos((transform.position.x + Time.time * speed) * frequenz) * frequenz * WaveHeight;
+        float newXPos = transform.position.x + speed * Time.deltaTime;
+        float newY = transform.position.y + newYPos - (yAbleitung * slopeSpeedFactor * Time.deltaTime);
+        transform.position = new Vector3(newXPos, newY, transform.position.z);
     }
 }
