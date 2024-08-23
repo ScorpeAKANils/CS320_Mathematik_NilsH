2f68156bfc3817a757e969801f583d7fb4f1b9a6
nilsh <nils.henningsen@freenet.de>
2024-08-23 16:50:44 +0200

    cooler stuff

--- Assets/MathePruefung.cs
+++ Assets/MathePruefung.cs
@@ -107,11 +107,9 @@ public class MathePruefung : MonoBehaviour
         float xPos = transform.position.x + speed * Time.deltaTime;
         float zPos = transform.position.z + speed * Time.deltaTime;
         float yPos = Mathf.Cos(xPos + Time.time) * (0.25f * xPos) + Mathf.Sin(zPos + Time.time) * WaveHeight;
+        
         float yAbleitungX = -Mathf.Sin(xPos + Time.time) * (0.25f * xPos) * WaveHeight;
         float yAbleitungZ = Mathf.Cos(zPos + Time.time) * WaveHeight;
-
-        float newRotationX = Mathf.Atan2(yAbleitungZ, 1) * Mathf.Rad2Deg;
-        float newRotationZ = Mathf.Atan2(yAbleitungX, 1) * Mathf.Rad2Deg;
         float newY = yPos - (yAbleitungX * slopeSpeedFactor * Time.deltaTime) - (yAbleitungZ * slopeSpeedFactor * Time.deltaTime);
 
         Vector3 lastPos = transform.position;
